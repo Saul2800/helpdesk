ed924f6f946b3dff701a56550caf090fe9d6749c
Roberto Neri Navarro <75956181+RobertoNeri@users.noreply.github.com>
2021-02-16 14:40:01 -0600

    Actualización de sideebar Ayuda

--- sidebar.php
+++ sidebar.php
@@ -26,7 +26,7 @@
                     </li>
 
                     <li class="<?php if(isset($active8)){echo $active8;}?>">
-                        <a href="about.php"><i class="fa fa-child"></i> Sobre Mi</a>
+                        <a href="about.php"><i class="fa fa-child"></i> Ayuda</a>
                     </li>
 
                 </ul>
