71626f47d9171fb1a235da9b4022c7793aec9a1e
joseluisic <jcaamal@kaabcode.com>
2021-02-24 11:25:18 -0600

    Se añaden las plantillas para guardar las imagenes adjuntas

---
+++ action/upload-img-uno.php
@@ -0,0 +1,31 @@
+<?php
+include "../config/config.php";
+
+if (isset($_FILES["file"]))
+{
+    $file = $_FILES["file"];
+    $name = $file["name"];
+    $type = $file["type"];
+    $tmp_n = $file["tmp_name"];
+    $size = $file["size"];
+    $folder = "../images/profiles/";
+    
+    if ($type != 'image/jpg' && $type != 'image/jpeg' && $type != 'image/png' && $type != 'image/gif')
+    {
+      echo "Error, el archivo no es una imagen"; 
+    }
+    else if ($size > 1024*1024)
+    {
+      echo "Error, el tamaño máximo permitido es un 1MB";
+    }
+    else
+    {
+        $src = $folder.$name;
+       @move_uploaded_file($tmp_n, $src);
+
+       $query=mysqli_query($con, "UPDATE user set profile_pic=\"$name\"");
+       if($query){
+        echo "<META HTTP-EQUIV='REFRESH' CONTENT='0;URL=dashboard.php'>";
+       }
+    }
+}
