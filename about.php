9bd99fff0ac77981bfafd30e0c2c99d873f46512
Roberto Neri Navarro <75956181+RobertoNeri@users.noreply.github.com>
2021-02-27 13:28:44 -0600

    Añadida la pagina de Privacidad, correccion a al apartado Ayuda en el cual solo hace faltan las preguntas para el FAQ, pero tiene la estructura para colocarse

--- about.php
+++ about.php
@@ -3,45 +3,48 @@
     include "head.php";
     include "sidebar.php";  
 ?>
-
+    <head>
+        <link href="css/estilosnuevos.css" rel="stylesheet">
+</head>
     <div class="right_col" role="main"> <!-- page content -->
         <div class="">
             <div class="page-title">
             <br>
                 <div class="row">
             	    <div class="col-md-6 col-md-offset-3">
-                        <h1 style="text-align:center">Acerca de HelpDesk</h1>
-                        <p>Hola, soy <strong><a target="_blank" href=""></a></strong> (Abizoft) creador de <a>ticketly</a>.</p>
+                        <div style="margin:20px">
+                            <h3 style="text-align:center; padding:16px;">COMO DAR DE ALTA UN TICKET</h3>
+                            <p>¡Bienvenid@ al apartado de AYUDA!, Aqui le explicaremos cómo dar de alta un ticket
+                            y como dar seguimiento a un reporte.</p>
+                            <h3 style="text-align:center; padding:16px;">Pasos</h3>
+                            <h2 style="text-align:center;">Alta de ticket</h2>
+                            
+                                <li style="text-align:justify"><b>1.   </b>Para dar de alta un ticket nos dirigimos al apartado de "Tickets"</li>
+                                <img src="images/ayuda/1.jpg" style="text-align:center; margin:20px; border:#a33532 4px solid; width:80%;">
+                                <li style="text-align:justify"><b>2.   </b>En la esquina superior izquierda, damos click en el botón "Agregar Ticket": Se abrirá un apartado para generar nuestro ticket</li>
+                                <img src="images/ayuda/2.jpg" style="text-align:center; margin:20px; border:#a33532 4px solid; width:80%;">
+                                <li style="text-align:justify"><b>3.   </b>Dentro del apartado llenamos los campos segun sea nuestro caso y una vez hecho esto, Guardamos.</li>
+                                <img src="images/ayuda/3.jpg" style="text-align:center; margin:20px; border:#a33532 4px solid; width:80%;">
+                                <h2 style="text-align:center;">Seguimiento de Reporte</h2>
+                                <li style="text-align:justify; padding-bottom:16px;"><b>1.   </b>Dentro del mismo apartado, en la zona central, podemos buscar nuestro proyecto o ubicarlo
+                                en la ventana de tickets abiertos, en donde se nos mostraran de forma ordenada. </li>                
+                           
+                                <div style=" padding: 16px; border:#a33532 4px solid;">
+                                    <h1 style="text-align:center;">FAQ</h1>
+                                    <h3 style="text-align:center;">Preguntas Frecuentes</h3>
+                                    <li style="text-align:justify; padding-top:16px;"><h4>¿Neri?</h4></li>
+                                    <li style="text-align:justify; "><h6>seh</h6></li>
+                                    <li style="text-align:justify; padding-top:16px;"><h4>¿Neri?</h4></li>
+                                    <li style="text-align:justify; "><h6>seh</h6></li>
+                                    <li style="text-align:justify; padding-top:16px;"><h4>¿Neri?</h4></li>
+                                    <li style="text-align:justify; "><h6>seh</h6></li>
+                                    <li style="text-align:justify; padding-top:16px;"><h4>¿Neri?</h4></li>
+                                    <li style="text-align:justify; "><h6>seh</h6></li>
+                                    <li style="text-align:justify; padding-top:16px;"><h4>¿Neri?</h4></li>
+                                    <li style="text-align:justify; "><h6>seh</h6></li>
 
-                        <h2>TICKETLY - <small>Sistema de Tickets de Soporte</small></h2>
-                        <p style="text-align:justify">TICKETLY es un sistema de Tickets de soporte de proposito general, codigo abierto y gratis.</p>
-
-                        <h1 style="text-align: center; padding:16px;">TICKETLY</h1>
-                        <h3 style="text-align:center">Descripcion</h3>
-                        <p style="text-align:justify">Ticketly es un sistema para gestionar tickets de soporte usando proyectos, categorias, prioridades y mas, ideal para todo tipo de negocios, desarrollo de proyectos, gestion personal, etc, el sistema esta desarrollado con PHP y MySQL.</p>
-                        <p style="text-align:justify">TICKETLY es un sistema de tickets de soporte en el que puedes rellenar tu perfil, agregar ticket, proyectos y categorias, generar reportes y mucho mas.</p>
-
-                        <h3 style="text-align:center; padding:16px;">Modulos</h3>
-                        <p>Defino a grandes rasgos los modulos generales del sistema</p>
-                        <ul>
-                        <li><b>Usuarios</b>: Se puede agregar editar y eliminar usuarios.</li>
-                        <li><b>Tickets</b>: Cada usuario puede agregar un ticket.</li>
-                        <li><b>Proyectos</b>: Cada usuario puede agregar los proyectos que desee.</li>
-                        <li><b>Perfiles</b>: Los usuarios pueden rellenar su perfil</li>
-                        <li><b>Categorias</b>: Los usuarios agregar categorias.</li>
-                        <li><b>Reportes</b>: Los usuarios pueden generar reportes.</li>
-                        </ul>
-
-                        <br><br>
-                        <p>Si quieres mejoras sobre este sistema no dudes en ponerte en contacto conmigo (waptoing7@gmail.com).</p>
-                        <p>EL sistema es de codigo abierto, puede ser utilizado para empresas o uso personal.</p>
-                        
-                        <h3>Apoyame!</h3>
-                        <p>Hay muchas formas en las que me puedes apoyar para seguir creando contenido, proyectos y actualizaciones a los sistemas</p>
-
-
-                        <h1 style="text-align: center;"><a href="http://github.com/amnersaucedososa" target="_blank">ABISOFT</a></h1>
-                        -->
+                                </div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ legal.php
@@ -0,0 +1,28 @@
+<head>
+<link href="css/legal.css" rel="stylesheet">
+<link rel ="icon" type="icon/png" href="images/iconoPestaña.png" > <!--Icono de pestaña-->
+<title>Aviso de Privacidad</title>
+</head>
+<div class="top">
+    <a href="dashboard.php">
+        <p>HelpDesk</p>
+    </a>
+    <div class="imagen">
+        <img src="images/iconoPestaña.png">
+    </div>
+</div>
+<div class="contentBox">
+    <div class="contenedorTexto">
+        <div style="padding:64px;">
+        <h5>Última Modificación: 26 de Febrero del 2021</h5>    
+        <h1> Aviso de Privacidad </h1>
+
+        
+        <h3>Aviso de privacidad de Help Desk</h3><br>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae a, ipsam doloremque, expedita nam vel veritatis incidunt eaque iste ex quaerat nihil! Aut deleniti voluptas facere eius explicabo. Ea, eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae a, ipsam doloremque, expedita nam vel veritatis incidunt eaque iste ex quaerat nihil! Aut deleniti voluptas facere eius explicabo. Ea, eaque!</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae a, ipsam doloremque, expedita nam vel veritatis incidunt eaque iste ex quaerat nihil! Aut deleniti voluptas facere eius explicabo. Ea, eaque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae a, ipsam doloremque, expedita nam vel veritatis incidunt eaque iste ex quaerat nihil! Aut deleniti voluptas facere eius explicabo. Ea, eaque!</p>
+        </div>
+    </div>
+</div>
