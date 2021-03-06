3e83fd231a10c5bddc187d4d16b6fba6d50cba69
Saul Arenas <Saul@MacBooks-MBP.lan>
2021-03-05 22:07:10 -0600

    Se agrego una carpeta para subir imagenes de los tickets esto aun no se cumple, tambien se modifico el formulario de aniadir tickets para que solo el admin pueda asignar un ticket a los que no sean usuarios o administradores.

--- action/upload-img-uno.php
+++ action/upload-img-uno.php
@@ -1,14 +1,14 @@
 <?php
 include "../config/config.php";
 
-if (isset($_FILES["file"]))
+if (isset($_FILES["file1"]))
 {
-    $file = $_FILES["file"];
+    $file = $_FILES["file1"];
     $name = $file["name"];
     $type = $file["type"];
     $tmp_n = $file["tmp_name"];
     $size = $file["size"];
-    $folder = "../images/profiles/";
+    $folder = "../images/helpTicket/";
     
     if ($type != 'image/jpg' && $type != 'image/jpeg' && $type != 'image/png' && $type != 'image/gif')
     {
@@ -23,9 +23,10 @@ if (isset($_FILES["file"]))
         $src = $folder.$name;
        @move_uploaded_file($tmp_n, $src);
 
-       $query=mysqli_query($con, "UPDATE user set profile_pic=\"$name\"");
+       $query=mysqli_query($con, "UPDATE ticket set problem_imguno=\"$name\"");
        if($query){
         echo "<META HTTP-EQUIV='REFRESH' CONTENT='0;URL=dashboard.php'>";
        }
     }
 }
+?>
