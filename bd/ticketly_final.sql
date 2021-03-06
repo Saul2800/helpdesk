44cdb7e41f436745568cd76377aea63e41e3f370
Saul Arenas <Saul@MacBooks-MBP.lan>
2021-03-05 22:53:57 -0600

    se modifico la bd

--- bd/ticketly_final.sql
+++ bd/ticketly_final.sql
@@ -47,7 +47,7 @@ CREATE TABLE `comment` (
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
