a51b7165656f9fec0c02ee39dd7fc9b585413c3a
joseluisic <jcaamal@kaabcode.com>
2021-03-08 15:05:10 -0600

    Se configura el mailer con los parametros del servidor Hostinger

--- testmailer.php
+++ testmailer.php
@@ -1,5 +1,6 @@
 <?php
-/*Clase que inicia el phpMailer para el envío de correos; Jose Luis Caamal Ic 08/03/2021*/
+/*Clase que inicia el phpMailer para el envío de correos; Jose Luis Caamal Ic 08/03/2021
+Actualización, ya configurado para el servidor :D*/
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -10,33 +11,34 @@ require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 require_once "vendor/autoload.php";
 
 $mail = new PHPMailer(true);
-
+$mail->CharSet = 'UTF-8';
 //Enable SMTP debugging.
 $mail->SMTPDebug = 3;   //Esto se comenta para no mostrar el debug en producción                            
 //Set PHPMailer to use SMTP.
 $mail->isSMTP();            
 //Set SMTP host name                          
-$mail->Host = "smtp.gmail.com";
+//$mail->Host = "smtp.gmail.com"; Host Google
+$mail->Host = "smtp.hostinger.mx"; //Servidor Hostinger
 //Set this to true if SMTP host requires authentication to send email
 $mail->SMTPAuth = true;                          
 //Provide username and password     
-$mail->Username = "joseluisitmerida93@gmail.com";                 
-$mail->Password = "*****"; //Aquí va la constraseña                          
+$mail->Username = "info@helpdesk.kaabcode.com";                 
+$mail->Password = "Kaabcode2021$"; //Aquí va la constraseña del que enviará                         
 //If SMTP requires TLS encryption then set it
 $mail->SMTPSecure = "tls";                           
 //Set TCP port to connect to
-$mail->Port = 587;     //El puerto                              
+$mail->Port = 587;     //El puerto   SMTP                           
 
-$mail->From = "joseluisitmerida93@gmail.com";//Correo del usuario que recibe
-$mail->FromName = "Full Name"; //Nombre del usuario que recibe
+$mail->From = "info@helpdesk.kaabcode.com";//Correo del usuario que envia
+$mail->FromName = "Información HelpDesk"; //Nombre del usuario que envia
 
-$mail->addAddress("jose.caamal@alumnos.udg.mx", "Recepient Name"); 
-
+$mail->addAddress("jose.caamal@alumnos.udg.mx", "Recepient Name");  //Correo del que recibe
 $mail->isHTML(true);
 
-$mail->Subject = "Subject Text"; //Aquí va el titulo del correo
+$mail->Subject = "Notificación Portal HelpDesk"; //Aquí va el titulo del correo
 $mail->Body = "<i>Esto es una prueba.</i>"; //Cuerpo del correo
 $mail->AltBody = "Esto es una prueba.";
+
 /*Se imprime en caso de tener errores*/
 try {
     $mail->send();
