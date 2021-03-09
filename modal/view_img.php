5e64761ccb14565f9ab9d0d6255e4cf5ceedb194
Saul Arenas <Saul@MacBooks-MBP.lan>
2021-03-08 19:13:08 -0600

    Se aniadio un archivo para la visualizacion de las imagenes de los tickets(aun en proceso)

---
+++ modal/view_img.php
@@ -0,0 +1,25 @@
+<?php
+    $projects =mysqli_query($con, "select * from project");
+    $priorities =mysqli_query($con, "select * from priority");
+    $statuses =mysqli_query($con, "select * from status");
+    $kinds =mysqli_query($con, "select * from kind");
+    $categories =mysqli_query($con, "select * from category");
+?>
+    <!-- Modal -->
+    <div class="modal fade bs-example-modal-lg-udp" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel"> Editar Ticket</h4>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /Modal -->
