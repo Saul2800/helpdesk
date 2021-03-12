0c3bc8ef18584436cd613d53c281d776c39b8f80
Saul Arenas <Saul@MacBooks-MBP.lan>
2021-03-12 14:30:05 -0600

    Se ajusta el modal para que se puedan visualizar las imagenes de los tickets, aun falta que se muetren las del ticket seleccionado

--- modal/view_img.php
+++ modal/view_img.php
@@ -1,25 +1,38 @@
 <?php
-    $projects =mysqli_query($con, "select * from project");
-    $priorities =mysqli_query($con, "select * from priority");
-    $statuses =mysqli_query($con, "select * from status");
-    $kinds =mysqli_query($con, "select * from kind");
-    $categories =mysqli_query($con, "select * from category");
+    $ticket_id=$_SESSION["id_ticket_TK"];//viene de ajax/tickets.php
+   $query1 = mysqli_query($con,"SELECT * FROM ticket WHERE id = \"$ticket_id\";");
+        if ($row = mysqli_fetch_array($query1)) {
+                $img_ticket1 = $row['problem_imguno'];
+                $img_ticket2 = $row['problem_imgdos'];
+        }
 ?>
-    <!-- Modal -->
-    <div class="modal fade bs-example-modal-lg-udp" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-md">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-                    </button>
-                    <h4 class="modal-title" id="myModalLabel"> Editar Ticket</h4>
-                </div>
-                <div class="modal-body">
-                    
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                </div>
-            </div>
+<div id="imgModal" class="modal fade mis-modales" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal-dialog modal-lg modal-blue">
+  <!-- Contenido del modal -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <button type="button" class="close" onclick="location.reload();" data-dismiss="modal">
+        &times;
+      </button>
+      <h2 class="modal-title" id="exampleModalLabel">
+        <font color="#a33532">Imagen del ticket</font>
+      </h2>
+    </div>
+    <div class="modal-body">
+    <div id="show_Images">
+    <div class="col-md-6">
+    <br><br>
+    <img class="thumb-image" style="width: 100%; display: block;" src="images/helpTicket/<?php echo $img_ticket1; ?>" alt="image1" />
+    </div>
+    <div class="col-md-6">
+    <br><br>
+    <img class="thumb-image" style="width: 100%; display: block;" src="images/helpTicket/<?php echo $img_ticket2; ?>" alt="image2" />
+    </div>
+    </div>
         </div>
-    </div> <!-- /Modal -->
+    <div class="modal-footer ">
+    </div>
+  </div>
+</div>
+</div>
+<!--Termina Modal -->
